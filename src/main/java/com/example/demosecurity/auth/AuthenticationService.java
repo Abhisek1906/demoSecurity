36926052d636a2a4ba26464011382089d250a368
Abhisek1906 <abhisekmishra150@gmail.com>
2023-03-21 12:46:02 +0530

    Initial commit

---
+++ src/main/java/com/example/demosecurity/auth/AuthenticationService.java
@@ -0,0 +1,57 @@
+package com.example.demosecurity.auth;
+
+import com.example.demosecurity.Config.JwtService;
+import com.example.demosecurity.user.Role;
+import com.example.demosecurity.user.User;
+import com.example.demosecurity.user.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepo repo;
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request) {
+
+
+        var user= User.builder()
+                .firstname((request.getFirstname()))
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repo.save(user);
+        var jwtToken= jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+        // If the user is a valid user then only give the token.
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user=repo.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken= jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/com/example/demosecurity/user/Role.java
@@ -0,0 +1,7 @@
+package com.example.demosecurity.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
